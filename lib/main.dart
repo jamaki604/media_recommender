37f62adc77ad4750b7068ca81f54293677b9da78
King <motokingjr@sbcglobal.net>
2023-10-27 19:23:24 -0400

    Updated main with skeleton for UI.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:media_recommender/parse_results.dart';
+import 'package:media_recommender/spotify_query_parser.dart';
+import 'package:media_recommender/track.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -7,46 +11,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Spotify Track Finder',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Spotify Track Finder'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +34,105 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String displayedText = '';
+  final trackController = TextEditingController();
+  final spotifyQueryParser = SpotifyQueryParser();
+  bool isLoading = false;
 
-  void _incrementCounter() {
+  Future<void> handleButtonPress() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      isLoading = true;
+      displayedText = '';
     });
+
+    try {
+      final result = await fetchTracks();
+      setState(() {
+        if (result.tracks.isEmpty) {
+          displayedText = "No tracks found.";
+        } else {
+          displayedText = formatTracks(result.tracks);
+        }
+        isLoading = false;
+      });
+    } catch (error) {
+      setState(() {
+        displayedText = "An error occurred: $error";
+        isLoading = false;
+      });
+    }
+  }
+
+  String formatTracks(List<Track> tracks) {
+    final int length = tracks.length > 10 ? 10 : tracks.length;
+    String result = '';
+    for (int i = 0; i < length; i++) {
+      result += "${i + 1}. ${tracks[i].name}\n";
+    }
+    return result;
+  }
+
+  Future<ParseResult> fetchTracks() async {
+    // Here, you'd use the trackController.text to fetch JSON data from the Spotify API.
+    // For now, I'll use a dummy json string to mimic that process.
+    final dummyJsonData = '{"tracks": {"items": [{"name": "Track 1"}, {"name": "Track 2"}, ...]}}'; // ... represents the other dummy tracks.
+    return spotifyQueryParser.parseJson(dummyJsonData);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(10.0),
+                color: Colors.green,
+                child: const Text(
+                  'Enter the search term for a Spotify track list',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: 345,
+                child: TextField(
+                  controller: trackController,
+                  keyboardType: TextInputType.text,
+                  enabled: !isLoading,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Search term goes here",
+                  ),
+                  style: Theme.of(context).textTheme.titleLarge, // Assuming titleLarge is defined in your theme.
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: isLoading ? null : handleButtonPress,
+                child: const Text('Submit'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  displayedText,
+                  style: const TextStyle(fontSize: 24),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
