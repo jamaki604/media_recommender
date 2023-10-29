a2933b724cefe0a1e911ee00e05bed6445bac2b1
King <motokingjr@sbcglobal.net>
2023-10-29 14:02:56 -0400

    Updated spotify test to obtain the spotify token to also store it. Wrote token_storage.dart to make it work.

--- test/spotfiy_test.dart
+++ test/spotfiy_test.dart
@@ -1,11 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:media_recommender/spotify_authorization.dart';
+import 'package:media_recommender/token_storage.dart';
 
 void main() {
   final auth = SpotifyAuthorization();
+  final storage = TokenStorage();
 
-  test('Obtain Spotify access token', () async {
+  test('Obtain and store Spotify access token', () async {
     final accessToken = await auth.getSpotifyAccessToken();
     expect(accessToken, isNotNull, reason: 'Failed to get the Spotify access token.');
+
+    await storage.storeToken(accessToken!);
+
+    final retrievedToken = await storage.retrieveToken();
+    expect(retrievedToken, isNotNull, reason: 'Failed to retrieve the token from storage.');
   });
 }
