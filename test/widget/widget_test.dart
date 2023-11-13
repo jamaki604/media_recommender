41bf428868caa7a24f9cf5905784e85bb3763dc5
Jake King <motokingjr@sbcglobal.net>
2023-11-13 17:24:40 -0500

    Update widget test
    Updated test to simplify it so it isn't based on build mode.

---
+++ test/widget/widget_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:media_recommender/main.dart';
+
+void main() {
+  testWidgets('MyApp loads MyHomePage with correct title',
+      (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify if 'Media Recommender' is in the app bar title.
+    expect(find.text('Media Recommender'), findsOneWidget);
+  });
+}
