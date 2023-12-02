f860be790d6826232c0f3f5a283a89f6ec64e63b
Jake King <motokingjr@sbcglobal.net>
2023-12-01 19:25:17 -0500

    Remove named constructors from SpotifySearchResult
    Remove named constructors and updated main constructor to take optional values.

--- lib/models/spotify_search_results.dart
+++ lib/models/spotify_search_results.dart
@@ -7,79 +7,30 @@ import 'package:media_recommender/models/show.dart';
 import 'package:media_recommender/models/track.dart';
 
 class SpotifySearchResults {
-  final List<Track>? _tracks;
-  final List<Album>? _albums;
-  final List<Artist>? _artists;
-  final List<Audiobook>? _audiobooks;
-  final List<Episode>? _episodes;
-  final List<Playlist>? _playlists;
-  final List<Show>? _shows;
+  List<Track>? _tracks;
+  List<Album>? _albums;
+  List<Artist>? _artists;
+  List<Audiobook>? _audiobooks;
+  List<Episode>? _episodes;
+  List<Playlist>? _playlists;
+  List<Show>? _shows;
 
-  SpotifySearchResults.tracks(tracks)
-      : _tracks = tracks,
-        _albums = null,
-        _artists = null,
-        _audiobooks = null,
-        _episodes = null,
-        _playlists = null,
-        _shows = null;
-
-  SpotifySearchResults.albums(albums)
-      : _albums = albums,
-        _tracks = null,
-        _artists = null,
-        _audiobooks = null,
-        _episodes = null,
-        _playlists = null,
-        _shows = null;
-
-  SpotifySearchResults.artists(artists)
-      : _artists = artists,
-        _tracks = null,
-        _albums = null,
-        _audiobooks = null,
-        _episodes = null,
-        _playlists = null,
-        _shows = null;
-
-  SpotifySearchResults.audiobooks(audiobooks)
-      : _audiobooks = audiobooks,
-        _tracks = null,
-        _albums = null,
-        _artists = null,
-        _episodes = null,
-        _playlists = null,
-        _shows = null;
-
-  SpotifySearchResults.episodes(episodes)
-      : _episodes = episodes,
-        _tracks = null,
-        _albums = null,
-        _artists = null,
-        _audiobooks = null,
-        _playlists = null,
-        _shows = null;
-
-  SpotifySearchResults.playlists(playlists)
-      : _playlists = playlists,
-        _tracks = null,
-        _albums = null,
-        _artists = null,
-        _audiobooks = null,
-        _episodes = null,
-        _shows = null;
-
-  SpotifySearchResults.shows(shows)
-      : _shows = shows,
-        _tracks = null,
-        _albums = null,
-        _artists = null,
-        _audiobooks = null,
-        _episodes = null,
-        _playlists = null;
-
-  SpotifySearchResults(this._tracks, this._albums, this._artists,
-      this._audiobooks, this._episodes, this._playlists, this._shows);
+  SpotifySearchResults(
+      List<Track>? tracks,
+      List<Album>? albums,
+      List<Artist>? artists,
+      List<Audiobook>? audiobooks,
+      List<Episode>? episodes,
+      List<Playlist>? playlists,
+      List<Show>? shows) {
+    _tracks = tracks;
+    _albums = albums;
+    _artists = artists;
+    _audiobooks = audiobooks;
+    _episodes = episodes;
+    _playlists = playlists;
+    _shows = shows;
+  }
 
   List<Track>? get tracks => _tracks;
 
